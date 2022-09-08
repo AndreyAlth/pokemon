3b6b15c57237b32f5f6c7d9c7f46caa7472da028
AndreyAlth <andreyalth@gmail.com>
2022-09-08 13:20:16 -0500

    agregar dark theme

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import { NextUIProvider } from "@nextui-org/react";
+import {darkTheme} from '../themes'
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div>
       <h1>Hola mundo</h1>
+      <Button>Hola</Button>
     </div>
   )
 }
