3b1a38ea096a922ec2d7957e20ef2c06b3e10acb
Gabriel Barcelo <andreyalth@gmail.com>
2022-10-05 09:53:53 -0500

    agregar titulo opcional

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -5,7 +5,7 @@ function Layout({ children, title }) {
   return (
     <>
     <Head>
-        <title>{title}</title>
+        <title>{title || 'Pokemon App'}</title>
         <meta name='author' content='Gabriel Barcelo' />
         <meta name={`description' content='Información sobre el Pokemon ${title}`} />
         <meta name='keywords' content={`${title}, pokemon, pokedex`} />
