a41df086aa942a754c0d853411f48de1f3697521
Gabriel Barcelo <andreyalth@gmail.com>
2022-10-05 11:39:15 -0500

    agregar tailwind

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,22 +1,25 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from "next/head";
+import React from "react";
+
+import Navbar from "../ui/Navbar";
 
 function Layout({ children, title }) {
   return (
     <>
-    <Head>
-        <title>{title || 'Pokemon App'}</title>
-        <meta name='author' content='Gabriel Barcelo' />
-        <meta name={`description' content='Información sobre el Pokemon ${title}`} />
-        <meta name='keywords' content={`${title}, pokemon, pokedex`} />
-    </Head>
+      <Head>
+        <title>{title || "Pokemon App"}</title>
+        <meta name="author" content="Gabriel Barcelo" />
+        <meta
+          name={`description' content='Información sobre el Pokemon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+      </Head>
 
-    <main>
-        {children}
-    </main>
+      <Navbar />
 
+      <main>{children}</main>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
---
+++ components/ui/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex w-full items-center justify-center px-[20px] bg-red-500'>
+        hola
+    </div>
+  )
+}
+
+export default Navbar
