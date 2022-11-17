5a094965f7a4b3747923ac67d794ced44a5c3f3f
AndreyAlth <andreyalth@gmail.com>
2022-11-17 00:16:45 -0600

    agregando card de pokemon

--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/layouts/Layout";
+import pokeApi from "../../api/pokeApi";
+import { Card, Grid } from "@nextui-org/react";
 
-function PokemonPage({ id, name }) {
+function PokemonPage({ pokemon }) {
   const router = useRouter();
-
-  console.log(router);
 
   return (
     <Layout title={"pokemon"}>
-      <h1>
-        {id} - {name}
-      </h1>
+      <p>jj</p>
+      <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card isHoverable={true} css={{padding: '30px'}}>
+            <Card.Body>
+              <Card.Image
+              src={pokemon.sprites.other?.dreamworld.front_default || './no-image.png'}
+              alt={pokemon.name}
+              width="100%"
+              height={200}
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
   );
 }
@@ -19,34 +31,23 @@ function PokemonPage({ id, name }) {
 export default PokemonPage;
 
 export async function getStaticPaths(context) {
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+  // console.log(pokemons151)
   return {
-    paths: [
-      {
-        params: {
-          id: "1",
-        },
-      },
-      {
-        params: {
-          id: "2",
-        },
-      },
-      {
-        params: {
-          id: "3",
-        },
-      },
-    ],
+    paths: pokemons151.map((id) => ({
+      params: { id },
+    })),
     fallback: false,
   };
 }
 
-export async function getStaticProps(context) {
-  /* const { data } = await pokeApi.get("/pokemon?limit=151"); */
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  const { data } = await pokeApi.get(`/pokemon/${id}`);
+  console.log(data);
   return {
     props: {
-      id: 1,
-      name: "Bulbasur",
+      pokemon: data,
     },
   };
 }
