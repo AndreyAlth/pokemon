f9cddff11aaf5778a4a3b4453de235a8fd73901e
AndreyAlth <andreyalth@gmail.com>
2022-11-21 21:38:54 -0600

    agregar boton

--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -2,25 +2,41 @@ import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/layouts/Layout";
 import pokeApi from "../../api/pokeApi";
-import { Card, Grid } from "@nextui-org/react";
+import { Button, Card, Grid, Text } from "@nextui-org/react";
 
 function PokemonPage({ pokemon }) {
   const router = useRouter();
 
   return (
     <Layout title={"pokemon"}>
-      <p>jj</p>
       <Grid.Container css={{ marginTop: "5px" }} gap={2}>
         <Grid xs={12} sm={4}>
-          <Card isHoverable={true} css={{padding: '30px'}}>
+          <Card isHoverable={true} css={{ padding: "30px" }}>
             <Card.Body>
               <Card.Image
-              src={pokemon.sprites.other?.dreamworld.front_default || './no-image.png'}
-              alt={pokemon.name}
-              width="100%"
-              height={200}
+                src={
+                  pokemon.sprites.other?.dream_world.front_default ||
+                  "./no-image.png"
+                }
+                alt={pokemon.name}
+                width="100%"
+                height={200}
               />
+              {console.log(pokemon)}
             </Card.Body>
+          </Card>
+        </Grid>
+
+        <Grid xs={12} sm={4}>
+          <Card isHoverable={true} css={{ padding: "30px" }}>
+            <Card.Header
+              css={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Text h1 transform="capitalize">
+                {pokemon.name}
+              </Text>
+              <Button color="gradient" ghost>hi</Button>
+            </Card.Header>
           </Card>
         </Grid>
       </Grid.Container>
